3a0ab96b3a4e969d3854023281ff67e346e3c4d8
Chartok <m.bhimjee@outlook.com>
2023-06-03 11:09:47 -0500

    bare-bones

---
+++ server.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const express = require('express');
+const routes = require('./controllers');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
+app.listen(PORT, () => console.log('Now listening at http://localhost:3000/'));
